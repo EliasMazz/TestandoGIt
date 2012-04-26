1b2f7e1ff56c682f36ab4ff7c2e8a8773a0cf636
Elias Mazzocco <elias.mazzocco@gmail.com>
2012-04-26 01:08:52 -0300

    Comitando do note =)

---
+++ TeteGitNote/src/TesteGitNote2.java
@@ -0,0 +1,13 @@
+
+public class TesteGitNote2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	System.out.println("testando o git no note");
+	//ssh-dss AAAAB3NzaC1kc3MAAACBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAAAAFQCXYFCPFSMLzLKSuYKi64QL8Fgc9QAAAIEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoAAACAPA8qHhNZbG5nmmfm+gQUGIzC+jeRM6uvPQh8TBDItEsg0wPH/wcBHSddD8Btl6Dt9XPczAe2heFNctm2Z3LIkf5sLt6g0AjliHmzy7UYhnKcYCqtyBJcCBg5OxwaaU+DMOa4HrZ8HeRnQGQOq4esltQoZHbej14aqK77Kqh2WUE= DSA-1024
+
+	}
+
+}
