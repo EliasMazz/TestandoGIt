98058236378f3e2695f099fcf1df9544c5b24bd5
Elias Mazzocco <elias.mazzocco@gmail.com>
2012-04-26 02:45:10 -0300

    pelo note

--- TesteGit/src/Testando.java
+++ TesteGit/src/Testando.java
@@ -6,7 +6,7 @@ public class Testando {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("oi, teste 2, agora teste 3, note modificando, pc modificando novamente");
+		System.out.println("oi, teste 2, agora teste 3, note modificando, pc modificando novamente, novamente modificado pelo note");
 		//ssh-dss AAAAB3NzaC1kc3MAAACBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAAAAFQCXYFCPFSMLzLKSuYKi64QL8Fgc9QAAAIEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoAAACBAL2auMlpvzHVoaV3iWNPEVXe6Zfd3ZA+VkBkgRckdJIU6bV+5IKZHdDsXe3B1QJm2ekkVS1FwIFvNPmyIFiV3istDwUelYqPGnYYc9wmzcKztDJKV6B0bVFtCnTlV2iIZ47PzFBUimw37XoQxNO0uXCgoxocZira6oVHV3rt35HG DSA-1024
 
 	}
